1175385a307a150559a7763a97dcacd48d9c68b7
kimseungmin1 <min0107212@naver.com>
2022-11-05 22:58:36 +0900

    answer 2

--- egg/test.py
+++ egg/test.py
@@ -1,3 +1,11 @@
 # 문자열 뒤집기
 def solution(my_string):
     return my_string[::-1]
+
+# 아이스 아메리카노
+
+def solution(money):
+    answer = [0, 0]
+    answer[0] = money // 5500
+    answer[1] = int(money % 5500)
+    return answer
