ab52c163e2dc1c2dd980f0fa3aae332043125679
eunbillee <pask2202@gmail.com>
2022-11-07 00:07:56 +0900

    solve_피자 나눠 먹기 (3)

---
+++ silver/programmers/day2/피자_나눠_먹기_(3).py
@@ -0,0 +1,9 @@
+# 1.
+def solution(slice, n):
+    if n % slice == 0:
+        return n // slice
+    else:
+        return n // slice + 1
+# 2.
+def solution(slice, n):
+    return ((n - 1) // slice) + 1
