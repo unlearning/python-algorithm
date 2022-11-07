75ec8a4f3626e118a24d62f61fc7f811f4812c30
kimseungmin1 <min0107212@naver.com>
2022-11-07 15:20:57 +0900

    # 피자 나눠 먹기

--- egg/test.py
+++ egg/test.py
@@ -9,3 +9,13 @@ def solution(money):
     answer[0] = money // 5500
     answer[1] = int(money % 5500)
     return answer
+
+# 피자 나눠 먹기
+def solution(slice, n):
+
+    answer = 0
+    if n % slice == 0:
+        answer = n//slice
+    else :
+        answer = n//slice + 1
+    return answer
