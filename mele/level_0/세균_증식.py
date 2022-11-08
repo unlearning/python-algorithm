11c19cf0e4afc6862602e06163039dc53a04313f
Me1e <mele0404@naver.com>
2022-11-08 20:56:23 +0900

    review: 221107

--- mele/level_0/세균_증식.py
+++ mele/level_0/세균_증식.py
@@ -8,3 +8,10 @@ def solution(n, t):
 # TODO: 이해해보기
 def solution(n, t):
     return n << t
+
+# 은비's
+def solution(n, t):
+    total = n
+    for i in range(t):
+        total *= 2
+    return total
