6cf8f84cfe1da6ef4257d3114d8359bf23165f49
eunbillee <pask2202@gmail.com>
2022-11-10 00:55:44 +0900

    solve_대문자와 소문자

--- silver/programmers/day5/대문자와 소문자.py
+++ silver/programmers/day5/대문자와 소문자.py
@@ -14,4 +14,4 @@ def solution(my_string):
 
 # 3.
 def solution(my_string):
-    return ''.join([i.lower() if ord(i)<ord('a') else i.upper() for i in my_string ])
+    return ''.join([i.lower() if ord(i)<ord('a') else i.upper() for i in my_string])
