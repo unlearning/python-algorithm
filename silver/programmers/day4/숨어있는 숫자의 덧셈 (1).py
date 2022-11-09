2100515d96033877cdc6e1d0427b870098ab5e5e
eunbillee <pask2202@gmail.com>
2022-11-10 00:59:37 +0900

    solve_숨어있는 숫자의 덧셈 (1)

--- silver/programmers/day4/숨어있는 숫자의 덧셈 (1).py
+++ silver/programmers/day4/숨어있는 숫자의 덧셈 (1).py
@@ -5,6 +5,6 @@ def solution(my_string):
     answer = re.sub(r'[^0-9]', '', my_string)
     return sum(map(int, answer))
 
-# 2.
+# 2. # list(filter(lambda x: x ==1, [1,2,3]))
 def solution(my_string):
-    return sum(int(i) for i in my_string if i.isdigit()) # 정확한 실행 순서?
+    return sum(int(i) for i in my_string if i.isdigit()) # for문에 해당하는 각각의 원소가 if condition에 해당하는지, 아닌지
