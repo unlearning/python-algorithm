d7a4fb00d66a5759090a5d4c813b3665fedd927a
Yubin_Hwang <bnm0218@naver,com>
2022-11-10 12:17:42 +0900

    인덱스 바꾸기

---
+++ ppinppini/Day1/Day2/인덱스 바꾸기.py
@@ -0,0 +1,10 @@
+# 나의풀이
+def solution(my_string, num1, num2):
+    answer = list(my_string)
+    answer1=answer[num1]
+    answer[num1]=answer[num2]
+    answer[num2]=answer1
+   
+    
+    
+    return ''.join(answer)
