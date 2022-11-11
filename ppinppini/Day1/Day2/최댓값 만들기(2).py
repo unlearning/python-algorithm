427d77a019ea9a8a2740719b7d9b7d546966e9e0
Yubin_Hwang <bnm0218@naver,com>
2022-11-11 19:51:06 +0900

    모스부호(1)

--- ppinppini/Day1/Day2/최댓값 만들기(2).py
+++ ppinppini/Day1/Day2/최댓값 만들기(2).py
@@ -1,6 +1,11 @@
 # 나의풀이
-import random
-a=[1,2,-3,4,-5]
-random.randrange(a)
-1
+def solution(numbers):
+    answer = numbers[0] * numbers[1]
+    
+    for i in range(len(numbers)):
+        for j in range(i+1, len(numbers)):
+            if numbers[i]*numbers[j] > answer:
+                answer = numbers[i]*numbers[j]
+    
+    return answer
 
--- ppinppini/Day1/Day2/합성수 찾기.py
+++ ppinppini/Day1/Day2/합성수 찾기.py
@@ -1,9 +1,11 @@
 # 나의풀이
 def solution(n):
+    num= []
     answer = 0
-    for i in range(1,n+1):
-      if n%i ==0:
-        answer+=i
-      return (len(str(answer))>=3)
-
-print(solution(10))
+    for j in range (2,n+1):
+        for i in range(1,n+1):
+            if j%i ==0:
+                num.append(j)
+        if num.count(j)>=3:
+            answer+=1        
+    return answer
