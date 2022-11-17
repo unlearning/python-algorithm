499f5fce96e026b71ac455d03d85ad12598ab269
eunbillee <pask2202@gmail.com>
2022-11-18 00:01:41 +0900

    solve_합성수 찾기

--- silver/programmers/day6/합성수 찾기.py
+++ silver/programmers/day6/합성수 찾기.py
@@ -8,3 +8,13 @@ def solution(n):
                 count -= 1
                 break
     return n - 1 - count
+
+# 2.
+def solution(n):
+    output = 0
+    for i in range(4, n + 1):
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                output += 1
+                break
+    return output
