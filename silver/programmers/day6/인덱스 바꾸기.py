46e7bbe7b0b592ef5105f048d730847c4a79aaee
eunbillee <pask2202@gmail.com>
2022-11-18 00:01:06 +0900

    solve_인덱스 바꾸기

--- silver/programmers/day6/인덱스 바꾸기.py
+++ silver/programmers/day6/인덱스 바꾸기.py
@@ -2,4 +2,4 @@
 def solution(my_string, num1, num2):
     l_string = list(my_string)
     l_string[num1], l_string[num2] = l_string[num2], l_string[num1]
-    return ''.join(map(str, l_string)) # 리스트 → 문자열
+    return ''.join(l_string) # 리스트 → 문자열
